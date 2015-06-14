859e092725f24b3dafe9c8ea79ec55319e3fc85a
Allyson Julian <hckr@eml.cc>
2015-06-13 19:37:16 -0500

    Updated config.

--- publishconf.py
+++ publishconf.py
@@ -10,13 +10,13 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://astrohckr.bitbucket.org'
+SITEURL = 'http://astrohckr.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
-OUTPUT_RETENTION = ['.git', '.hg']
+OUTPUT_RETENTION = ['.git', '.hg', 'CNAME']
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
