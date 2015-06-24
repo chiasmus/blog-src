6cab1691c25c62cac7cf2244bfa410a644f7e1c2
Al Jul <hckr@eml.cc>
2015-06-24 05:23:43 +0000

    pelicanconf.py edited online with Bitbucket

--- pelicanconf.py
+++ pelicanconf.py
@@ -49,7 +49,7 @@ SOCIAL = (('Twitter', 'https://twitter.com/astrohckr'),)
 
 DEFAULT_PAGINATION = 10
 
-THEME = '/home/aj/Code/pelican-themes/pelican-svbhack'
+# THEME = '/home/aj/Code/pelican-themes/pelican-svbhack'
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
