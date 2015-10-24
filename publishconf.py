646f17ed82f88bc8f81bfc9da2e5b1749d9ad082
AJ <hckr@eml.cc>
2015-10-24 17:29:07 -0500

    Update url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://ajwrit.es'
+SITEURL = 'http://chiasmus.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
